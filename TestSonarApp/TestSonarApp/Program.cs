75726bdf5deabeafe98aa90fe91f965cecef914e
Andrei Charkasau <andrei.charkasau@innowise.com>
2026-03-16 17:14:35 +0300

    Update Program.cs

--- TestSonarApp/TestSonarApp/Program.cs
+++ TestSonarApp/TestSonarApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
 var a = 3;
-var b = 4;
+var b = 4; //Some blank comment to delete after the development (or right now!!!)
 if(a > b)
 {
   Console.WriteLine("A>B");
